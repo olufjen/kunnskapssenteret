5924e9fb55ed2b1dad4d0fdce7091e27794db3eb
leiftorger <leiftorger@e7bf3d78-9b82-11dd-8e58-870c635edf7a>
2008-11-12 17:29:54 +0000

    Altered package structure

---
+++ trunk/helsebiblioteket-domain-dao-services/src/no/helsebiblioteket/admin/service/AdminService.java
@@ -0,0 +1,14 @@
+package no.helsebiblioteket.admin.service;
+
+import java.io.Serializable;
+import java.util.List;
+
+import no.helsebiblioteket.admin.dao.UserDao;
+import no.helsebiblioteket.domain.User;
+
+public interface AdminService extends Serializable {
+    public void setUserDao(UserDao userDao);
+    public User findUserByUsername(User user);
+    public List<User> getUserList();
+    public void createUser(User user);
+}
