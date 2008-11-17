6e01d89326d6a58f4373cdd0693559548b9565a8
leiftorger <leiftorger@e7bf3d78-9b82-11dd-8e58-870c635edf7a>
2008-11-17 00:56:14 +0000

    Service and dao for accessing and storing data mainly related to dropdown lists, checkbox groups etc

---
+++ helsebiblioteket-domain-dao-services/src/no/helsebiblioteket/admin/dao/OrganizationTypeDao.java
@@ -0,0 +1,15 @@
+package no.helsebiblioteket.admin.dao;
+
+/**
+ * 
+ * @author ltg
+ * 
+ * Purpose of this class is retrieve and store types of organizations
+ */
+
+import java.util.List;
+import no.helsebiblioteket.admin.domain.OrganizationType;
+
+public interface OrganizationTypeDao {
+	public List<OrganizationType> getOrganizationTypeList();
+}
