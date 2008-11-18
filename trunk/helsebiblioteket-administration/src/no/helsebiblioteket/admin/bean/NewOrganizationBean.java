c10a9a170df7e75f5265185a0f06ee94ba341e16
leiftorger <leiftorger@e7bf3d78-9b82-11dd-8e58-870c635edf7a>
2008-11-18 16:00:40 +0000

    Functionality related to registering, changing and retrieving an organization

--- trunk/helsebiblioteket-administration/src/no/helsebiblioteket/admin/bean/NewOrganizationBean.java
+++ trunk/helsebiblioteket-administration/src/no/helsebiblioteket/admin/bean/NewOrganizationBean.java
@@ -3,31 +3,112 @@ package no.helsebiblioteket.admin.bean;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.faces.application.FacesMessage;
+import javax.faces.event.ActionEvent;
 import javax.faces.component.UIComponent;
 import javax.faces.component.UIInput;
-import javax.faces.component.UISelectItem;
+import javax.faces.component.html.HtmlDataTable;
 import javax.faces.context.FacesContext;
-import javax.faces.validator.ValidatorException;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.myfaces.custom.tree2.UITreeData;
 
+import no.helsebiblioteket.admin.domain.Contract;
+import no.helsebiblioteket.admin.domain.IpRange;
 import no.helsebiblioteket.admin.domain.Organization;
 import no.helsebiblioteket.admin.domain.OrganizationType;
 import no.helsebiblioteket.admin.domain.Person;
+import no.helsebiblioteket.admin.domain.IpRange;
 
 public class NewOrganizationBean {
 	protected final Log logger = LogFactory.getLog(getClass());
 	
 	private Organization organization = null;
+	private String organizationName = null;
 	private Person loggedInUser = null;
+	private String ipAddressSingle = null;
+	private String ipAddressFrom = null;
+	private String ipAddressTo = null;
+	private UIInput ipAddressSingleUIInput = null;
+	private UIInput ipAddressFromUIInput = null;
+	private UIInput ipAddressToUIInput = null;
+	private OrganizationType organizationType = null;
+	private String selectedOrganizationTypeId = null;
+	private List<IpRange> ipRangeList = null;
+	private List<Contract> contractsForOrganizationType = null;
 	
+	private HtmlDataTable ipRangeListHtmlDataTable = null;
+	
+	private List<Contract> choosenContracts = null;
+
 	public NewOrganizationBean() {
-		this.organization = new Organization();
-		this.organization.setType(new OrganizationType());
+		
 	}
 	
+	public String getOrganizationName() {
+		return organizationName;
+	}
+
+	public void setOrganizationName(String organizationName) {
+		this.organizationName = organizationName;
+	}
+
+	public String getSelectedOrganizationTypeId() {
+		return selectedOrganizationTypeId;
+	}
+
+	public void setSelectedOrganizationTypeId(String selectedOrganizationTypeId) {
+		this.selectedOrganizationTypeId = selectedOrganizationTypeId;
+	}
+
+	public String getIpAddressSingle() {
+		return ipAddressSingle;
+	}
+
+	public void setIpAddressSingle(String ipAddressSingle) {
+		this.ipAddressSingle = ipAddressSingle;
+	}
+
+	public UIInput getIpAddressSingleUIInput() {
+		return ipAddressSingleUIInput;
+	}
+
+	public void setIpAddressSingleUIInput(UIInput ipAddressSingleUIInput) {
+		this.ipAddressSingleUIInput = ipAddressSingleUIInput;
+	}
+
+	public String getIpAddressFrom() {
+		return ipAddressFrom;
+	}
+
+	public void setIpAddressFrom(String ipAddressFrom) {
+		this.ipAddressFrom = ipAddressFrom;
+	}
+
+	public String getIpAddressTo() {
+		return ipAddressTo;
+	}
+
+	public void setIpAddressTo(String ipAddressTo) {
+		this.ipAddressTo = ipAddressTo;
+	}
+	
+	public UIInput getIpAddressFromUIInput() {
+		return ipAddressFromUIInput;
+	}
+
+	public void setIpAddressFromUIInput(UIInput ipAddressFromUIInput) {
+		this.ipAddressFromUIInput = ipAddressFromUIInput;
+	}
+
+	public UIInput getIpAddressToUIInput() {
+		return ipAddressToUIInput;
+	}
+
+	public void setIpAddressToUIInput(UIInput ipAddressToUIInput) {
+		this.ipAddressToUIInput = ipAddressToUIInput;
+	}
+
 	public void setLoggedInUser(Person person) {
 		this.loggedInUser = person;
 	}
@@ -36,7 +117,61 @@ public class NewOrganizationBean {
 		return this.organization;
 	}
 	
-	public void save() {
-		logger.debug("Method 'save' invoked");
+	public void setOrganization(Organization organization) {
+		this.organization = organization;
+	}
+	
+	public OrganizationType getOrganizationType() {
+		return this.organizationType;
+	}
+	
+	public List<IpRange> getIpRangeList() {
+		return this.ipRangeList;
+	}
+
+	public HtmlDataTable getIpRangeListHtmlDataTable() {
+		return this.ipRangeListHtmlDataTable;
+	}
+
+	public void setIpRangeListHtmlDataTable(HtmlDataTable ipRangeListHtmlDataTable) {
+		this.ipRangeListHtmlDataTable = ipRangeListHtmlDataTable;
+	}
+
+	public void actionSaveOrganization() {
+		logger.debug("Method 'actionSaveOrganization' invoked");
+	}
+	
+	public void actionAddSingleIp() {
+		logger.debug("Method 'actionAddSingleIp' invoked");
+		setIpAddressSingle((getIpAddressSingleUIInput().getSubmittedValue() != null) ? getIpAddressSingleUIInput().getSubmittedValue().toString() : null);
+		if (this.ipRangeList == null) {
+			this.ipRangeList = new ArrayList<IpRange>();
+		}
+		this.ipRangeList.add(new IpRange(getIpAddressSingle(), null));
+	}
+	
+	public void actionAddIpRange() {
+		logger.debug("Method 'actionAddIpRange' invoked");
+		setIpAddressFrom((getIpAddressFromUIInput().getSubmittedValue() != null) ? getIpAddressFromUIInput().getSubmittedValue().toString() : null);
+		setIpAddressTo((getIpAddressToUIInput().getSubmittedValue() != null) ? getIpAddressToUIInput().getSubmittedValue().toString() : null);
+		if (this.ipRangeList == null) {
+			this.ipRangeList = new ArrayList<IpRange>();
+		}
+		this.ipRangeList.add(new IpRange(getIpAddressFrom(), getIpAddressTo()));
+	}
+	
+	public void actionDeleteIpRange() {
+		logger.debug("Method 'actionDeleteIpRange' invoked");
+		//Integer rowIndex = (Integer) FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("ipRangeDeleteTableRowIndex"); 
+		this.ipRangeList.remove((IpRange) this.ipRangeListHtmlDataTable.getRowData());
+	}
+	
+	public void addIpRangeActionListener(ActionEvent actionEvent) {
+		logger.debug("Method 'addIpRangeActionListener' invoked");
+		logger.debug("vce.getComponent().getId(): " + actionEvent.getComponent().getId());
+	}
+	
+	public boolean isShowIpRangeList() {
+		return (ipRangeList != null && ipRangeList.size() > 0) ? true : false;
 	}
 }
