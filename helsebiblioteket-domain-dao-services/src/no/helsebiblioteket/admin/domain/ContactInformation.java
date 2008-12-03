b587c72f4eb3684df1004da643ea398087ef0e5e
leiftorger <leiftorger@e7bf3d78-9b82-11dd-8e58-870c635edf7a>
2008-12-03 12:53:29 +0000

    added getters and setters

--- helsebiblioteket-domain-dao-services/src/no/helsebiblioteket/admin/domain/ContactInformation.java
+++ helsebiblioteket-domain-dao-services/src/no/helsebiblioteket/admin/domain/ContactInformation.java
@@ -1,16 +1,48 @@
 package no.helsebiblioteket.admin.domain;
 
 public class ContactInformation {
-//	private Integer id = null;
-//	private String postalAddress = null;
-//	private String postalCode = null;
-//	private String telephoneNumber= null;
-//	private String postalLocation = null;
+	private Integer id = null;
+	private String postalAddress = null;
+	private String postalCode = null;
+	private String telephoneNumber= null;
+	private String postalLocation = null;
 	private String email = null;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getPostalAddress() {
+		return postalAddress;
+	}
+	public void setPostalAddress(String postalAddress) {
+		this.postalAddress = postalAddress;
+	}
+	public String getPostalCode() {
+		return postalCode;
+	}
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+	public String getTelephoneNumber() {
+		return telephoneNumber;
+	}
+	public void setTelephoneNumber(String telephoneNumber) {
+		this.telephoneNumber = telephoneNumber;
+	}
+	public String getPostalLocation() {
+		return postalLocation;
+	}
+	public void setPostalLocation(String postalLocation) {
+		this.postalLocation = postalLocation;
+	}
 	public String getEmail() {
 		return email;
 	}
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
+	
 }
