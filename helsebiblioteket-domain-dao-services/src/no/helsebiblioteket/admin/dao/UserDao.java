d93a0299c0f0dffc62f49cf81a99c05f89a6060f
qadeeralvi <qadeeralvi@e7bf3d78-9b82-11dd-8e58-870c635edf7a>
2008-12-09 11:41:37 +0000

    spring security

--- helsebiblioteket-domain-dao-services/src/no/helsebiblioteket/admin/dao/UserDao.java
+++ helsebiblioteket-domain-dao-services/src/no/helsebiblioteket/admin/dao/UserDao.java
@@ -3,8 +3,9 @@ package no.helsebiblioteket.admin.dao;
 import no.helsebiblioteket.admin.domain.User;
 
 public interface UserDao {
+	public User findUser(String username);
 	public User getUserByUsername(User user);
-
 	public void insertUser(User user);
 	public void updateUser(User user);
+	
 }
