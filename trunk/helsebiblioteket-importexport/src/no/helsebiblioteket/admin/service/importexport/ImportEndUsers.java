1c721b60eb22ad8154634c730bcc7ea6d28628a5
leiftorger <leiftorger@e7bf3d78-9b82-11dd-8e58-870c635edf7a>
2008-12-22 12:21:58 +0000

    Share project "helsebiblioteket-importexport" into "https://kunnskapssenteret.googlecode.com/svn"
    Project for initial exports from old system and imports into new system

---
+++ trunk/helsebiblioteket-importexport/src/no/helsebiblioteket/admin/service/importexport/ImportEndUsers.java
@@ -0,0 +1,13 @@
+package no.helsebiblioteket.admin.service.importexport;
+
+/**
+ * This class:
+ * - fetches existing users from existing LDAP-server, 
+ * - stores the fetched users in new structure in a new database
+ * Users affected: All existing endusers: HPR, Stud, Emp - users.
+ * @author ltg
+ *
+ */
+public class ImportEndUsers {
+	
+}
