8e65a09881de78d0b19c69f92dbaceba735f1761
leiftorger <leiftorger@e7bf3d78-9b82-11dd-8e58-870c635edf7a>
2008-12-22 12:28:04 +0000

    Share project "helsebiblioteket-sqlmapdao" into "https://kunnskapssenteret.googlecode.com/svn" Ibatis sql maps

---
+++ helsebiblioteket-sqlmapdao/src/no/helsebiblioteket/admin/sqlmapdao/SqlMapOrganizationDao.java
@@ -0,0 +1,73 @@
+package no.helsebiblioteket.admin.sqlmapdao;
+
+import java.util.List;
+
+import org.springframework.orm.ibatis.support.SqlMapClientDaoSupport;
+
+import no.helsebiblioteket.admin.dao.OrganizationDao;
+import no.helsebiblioteket.admin.domain.ContactInformation;
+import no.helsebiblioteket.admin.domain.Organization;
+import no.helsebiblioteket.admin.domain.Person;
+import no.helsebiblioteket.admin.domain.SupplierOrganization;
+
+public class SqlMapOrganizationDao extends SqlMapClientDaoSupport implements OrganizationDao {
+
+	public void saveOrganization(Organization organization) {
+		if (organization.getId() != null) {
+			updateOrganization(organization);
+		} else {
+			insertOrganization(organization);
+		}
+	}
+	
+	private void updateOrganization(Organization organization) {
+		getSqlMapClientTemplate().update("updateOrganization", organization);
+		getSqlMapClientTemplate().update("updateContactPerson", organization);
+		getSqlMapClientTemplate().update("updateContactInformation", organization);
+		getSqlMapClientTemplate().update("updateAccessList", organization);
+		getSqlMapClientTemplate().update("updateIpRangeList", organization);
+		getSqlMapClientTemplate().update("updateNameList", organization);
+	}
+	
+	private void insertOrganization(Organization organization) {
+		if (organization.getContactPerson() != null) {
+			getSqlMapClientTemplate().update("insertPerson", organization.getContactPerson());
+		}
+		getSqlMapClientTemplate().update("insertOrganization", organization);
+		//getSqlMapClientTemplate().update("insertContactInformation", organization);
+		//getSqlMapClientTemplate().update("insertAccessList", organization);
+		//getSqlMapClientTemplate().update("insertIpRangeList", organization);
+		//getSqlMapClientTemplate().update("insertNameList", organization);
+	}
+	
+	public List<Organization> getAllOrganizations() {
+		List<Organization> organizationList = null;
+		organizationList = getSqlMapClientTemplate().queryForList("getOrganizationList");
+		for (Organization organization : organizationList) {
+			organization = populateOrganization(organization);
+		}
+		return organizationList;
+	}
+
+	public Organization getOrganization(Integer organizationId) {
+		Organization organization = null;
+		organization = (Organization) getSqlMapClientTemplate().queryForObject("getOrganizationById", organizationId);
+		organization = populateOrganization(organization);
+		return organization;
+	}
+	
+	private Organization populateOrganization(Organization organization) {
+		organization = (Organization) getSqlMapClientTemplate().queryForObject("getOrganizationById", organization.getId());
+		organization.setParent((Organization) getSqlMapClientTemplate().queryForObject("getParentOrganizationByOrganizationId", organization.getId()));
+		organization.setContactInformation((ContactInformation) getSqlMapClientTemplate().queryForObject("getContactInformationByOrganizationId", organization.getId()));
+		organization.setContactPerson((Person) getSqlMapClientTemplate().queryForObject("getContactPersonByOrganizationId", organization.getId()));
+		organization.setIpRangeList(getSqlMapClientTemplate().queryForList("getIpRangeListByOrganizationId", organization.getId()));
+		organization.setAccessList(getSqlMapClientTemplate().queryForList("getAccessListByOrganizationId", organization.getId()));
+		organization.setNameList(getSqlMapClientTemplate().queryForList("getNameListByOrganizationId", organization.getId()));	
+		return organization;
+	}
+
+	public List<SupplierOrganization> getSupplierList() {
+		return null;
+	}
+}
