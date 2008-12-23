31aca5342cfcd22a213714f5b4b3544ccee1fce7
qadeeralvi <qadeeralvi@e7bf3d78-9b82-11dd-8e58-870c635edf7a>
2008-12-23 15:26:27 +0000

    Import job for endusers

---
+++ helsebiblioteket-domain-dao-services/src/no/helsebiblioteket/admin/service/importexport/ImportEndUsersService.java
@@ -0,0 +1,19 @@
+package no.helsebiblioteket.admin.service.importexport;
+
+/**
+ * Purpose of this class is to retrieve and store static values used for 
+ * drop down lists, checkbox groups etc.
+ */
+
+import java.io.Serializable;
+import java.util.List;
+
+import no.helsebiblioteket.admin.domain.Position;
+import no.helsebiblioteket.admin.domain.Role;
+import no.helsebiblioteket.admin.domain.User;
+import no.helsebiblioteket.admin.request.PageRequest;
+import no.helsebiblioteket.admin.request.PageResult;
+
+public interface ImportEndUsersService extends Serializable {
+	public void importAllEndUsers();
+}
