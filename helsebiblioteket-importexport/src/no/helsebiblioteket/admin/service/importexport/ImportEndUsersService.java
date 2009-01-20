2bdd4509bcbbc71f8e20d3c0c9db407b3349c620
leiftorger <leiftorger@e7bf3d78-9b82-11dd-8e58-870c635edf7a>
2009-01-20 16:20:15 +0000

    refactoring

---
+++ helsebiblioteket-importexport/src/no/helsebiblioteket/admin/service/importexport/ImportEndUsersService.java
@@ -0,0 +1,7 @@
+package no.helsebiblioteket.admin.service.importexport;
+
+import java.io.Serializable;
+
+public interface ImportEndUsersService extends Serializable {
+	public void importAllEndUsers();
+}
