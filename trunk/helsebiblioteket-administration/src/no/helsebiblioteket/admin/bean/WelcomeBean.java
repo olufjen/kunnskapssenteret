46ebbf7882abcf33c29e7a3c347157de0cbeea40
leiftorger <leiftorger@e7bf3d78-9b82-11dd-8e58-870c635edf7a>
2009-01-25 14:59:15 +0000

    enabling importservices

--- trunk/helsebiblioteket-administration/src/no/helsebiblioteket/admin/bean/WelcomeBean.java
+++ trunk/helsebiblioteket-administration/src/no/helsebiblioteket/admin/bean/WelcomeBean.java
@@ -1,35 +1,35 @@
 package no.helsebiblioteket.admin.bean;
 
-//import no.helsebiblioteket.admin.service.importexport.ImportEndUsersService;
-//import no.helsebiblioteket.admin.service.importexport.ImportMemberOrganizationsService;
+import no.helsebiblioteket.admin.service.importexport.ImportEndUsersService;
+import no.helsebiblioteket.admin.service.importexport.ImportMemberOrganizationsService;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 public class WelcomeBean {
-//	private ImportEndUsersService importEndUsersService;
-//	private ImportMemberOrganizationsService importMemberOrganizationsService;
-//	
+	private ImportEndUsersService importEndUsersService;
+	private ImportMemberOrganizationsService importMemberOrganizationsService;
+	
 	/** Logger for this class and subclasses */
     protected final Log logger = LogFactory.getLog(getClass());
 	
-//    public void setImportEndUsersService(ImportEndUsersService importEndUsersService) {
-//    	this.importEndUsersService = importEndUsersService;
-//    }
+    public void setImportEndUsersService(ImportEndUsersService importEndUsersService) {
+    	this.importEndUsersService = importEndUsersService;
+    }
     
-//    public void setImportMemberOrganizationsService(ImportMemberOrganizationsService importMemberOrganizationsService) {
-//    	this.importMemberOrganizationsService = importMemberOrganizationsService;
-//    }
+    public void setImportMemberOrganizationsService(ImportMemberOrganizationsService importMemberOrganizationsService) {
+    	this.importMemberOrganizationsService = importMemberOrganizationsService;
+    }
     
 	public String actionImportAllEndUsers() {
 		logger.info("method 'actionImportAllEndUsers' invoked");
-//		importEndUsersService.importAllEndUsers();
+		importEndUsersService.importAllEndUsers();
 		return "welcome";
 	}
 	
 	public String actionImportMemberOrganizations() {
 		logger.info("method 'actionImportMemberOrganizations' invoked");
-//		importMemberOrganizationsService.importAllMemberOrganizations();
+		importMemberOrganizationsService.importAllMemberOrganizations();
 		return "welcome";
 	}
 }
