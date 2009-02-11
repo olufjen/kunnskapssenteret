d537b2c424e27df2293666165c86315cb61aeb67
leiftorger <leiftorger@e7bf3d78-9b82-11dd-8e58-870c635edf7a>
2009-02-11 14:25:18 +0000

    edit organization - work in progress

--- helsebiblioteket-administration/src/no/helsebiblioteket/admin/bean/OrganizationBean.java
+++ helsebiblioteket-administration/src/no/helsebiblioteket/admin/bean/OrganizationBean.java
@@ -8,6 +8,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import no.helsebiblioteket.admin.domain.Organization;
+import no.helsebiblioteket.admin.domain.key.OrganizationTypeKey;
 import no.helsebiblioteket.admin.domain.list.OrganizationListItem;
 import no.helsebiblioteket.admin.requestresult.FirstPageRequest;
 import no.helsebiblioteket.admin.requestresult.PageRequest;
@@ -37,11 +38,22 @@ public class OrganizationBean {
 		return "organization_details";
 //		else "organizations_overview";
 	}
-	public String actionEdit(){
-		this.organization = (Organization) this.organizationsTable.getRowData();
-		// FIXME: Pass this on to the edit bean!
-		return "organization_edit";
-//		else "organizations_overview";
+	public String actionEdit() {
+		String actionString = "organizations_overview";
+		setOrganization((Organization) this.organizationsTable.getRowData());
+		String orgTypeKey = this.organization.getType().getKey().toString();
+		if (orgTypeKey.equals(OrganizationTypeKey.content_supplier.toString())) {
+			actionString = "create_change_supplier_organization";
+		} else if (
+				orgTypeKey.equals(OrganizationTypeKey.health_enterprise.toString())
+				|| orgTypeKey.equals(OrganizationTypeKey.other.toString())
+				|| orgTypeKey.equals(OrganizationTypeKey.public_administration.toString())
+				|| orgTypeKey.equals(OrganizationTypeKey.teaching.toString())
+			) {
+			actionString = "create_change_member_organization";
+		}
+		logger.debug("changing organization with id " + this.organization.getId());
+		return actionString;
 	}
 	public void search() {
 		if(this.searchinput == null) { this.searchinput = ""; }
@@ -59,6 +71,10 @@ public class OrganizationBean {
 	public HtmlDataTable getOrganizationsTable() { return organizationsTable; }
 	public void setOrganizationsTable(HtmlDataTable organizationsTable) { this.organizationsTable = organizationsTable; }
 	public void setOrganizationService(OrganizationService organizationService) { this.organizationService = organizationService; }
+	
+	public void setOrganization(Organization organization) {
+		this.organization = organization;
+	}
 	public Organization getOrganization() {
 		return organization;
 	}
