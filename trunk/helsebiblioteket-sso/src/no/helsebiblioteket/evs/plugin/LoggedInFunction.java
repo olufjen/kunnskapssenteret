a5d77ca0c25d2244c49c9333b2f0ac6848b71bf5
leiftorger <leiftorger@e7bf3d78-9b82-11dd-8e58-870c635edf7a>
2009-02-12 10:03:43 +0000

    using dedicated "webservicemethod" for fetching positions

--- trunk/helsebiblioteket-sso/src/no/helsebiblioteket/evs/plugin/LoggedInFunction.java
+++ trunk/helsebiblioteket-sso/src/no/helsebiblioteket/evs/plugin/LoggedInFunction.java
@@ -20,6 +20,8 @@ import no.helsebiblioteket.admin.domain.MemberOrganization;
 import no.helsebiblioteket.admin.domain.Organization;
 import no.helsebiblioteket.admin.domain.Position;
 import no.helsebiblioteket.admin.domain.User;
+import no.helsebiblioteket.admin.requestresult.ListResult;
+import no.helsebiblioteket.admin.requestresult.ListResultPosition;
 import no.helsebiblioteket.admin.service.OrganizationService;
 import no.helsebiblioteket.admin.service.UserService;
 import no.helsebiblioteket.admin.translator.OrganizationToXMLTranslator;
@@ -109,7 +111,8 @@ public class LoggedInFunction{
 		PositionToXMLTranslator positionToXMLTranslator = new PositionToXMLTranslator();
 		org.w3c.dom.Document document = userTranslator.newDocument();
 		Element element = document.createElement("positions");
-		Position[] positions = this.userService.getPositionListAll("").getList();
+		ListResultPosition resultPositionList = this.userService.getPositionListAllWS("");
+		Position[] positions = resultPositionList.getList();
 		for (Position position : positions) {
 			positionToXMLTranslator.translate(position, document, element);
 		}
