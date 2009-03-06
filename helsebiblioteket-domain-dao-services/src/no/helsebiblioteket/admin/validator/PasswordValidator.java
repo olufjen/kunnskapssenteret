9937342c275aebd066fdc58af2ebf5030a6a436e
leiftorger <leiftorger@e7bf3d78-9b82-11dd-8e58-870c635edf7a>
2009-03-06 09:01:06 +0000

    Validator for passwords

---
+++ helsebiblioteket-domain-dao-services/src/no/helsebiblioteket/admin/validator/PasswordValidator.java
@@ -0,0 +1,27 @@
+package no.helsebiblioteket.admin.validator;
+
+import java.util.regex.Pattern;
+
+public class PasswordValidator {
+	private static PasswordValidator instance = null;
+	private static String validPasswordRegExpString = "[0-9A-Åa-å_]{6,}";
+	private static Pattern validPasswordRegExpPattern = null;
+	static {
+		validPasswordRegExpPattern = Pattern.compile(validPasswordRegExpString);
+	}
+	
+	protected PasswordValidator() {
+		// Exists only to defeat instantiation.
+	}
+	   
+	public static PasswordValidator getInstance() {
+		if(instance == null) {
+	    	instance = new PasswordValidator();
+		}
+	    return instance;
+	}
+	
+	public boolean isValidPassword(String password){
+		return validPasswordRegExpPattern.matcher(null == password ? "" : password).find();
+	}
+}
