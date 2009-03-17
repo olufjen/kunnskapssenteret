7e84e671aabd31786884aa2cb768b16e313d81d2
qadeeralvi <qadeeralvi@e7bf3d78-9b82-11dd-8e58-870c635edf7a>
2009-03-17 10:05:12 +0000

    cache Implementation

--- trunk/helsebiblioteket-test/src/no/helsebiblioteket/admin/test/BeanFactory.java
+++ trunk/helsebiblioteket-test/src/no/helsebiblioteket/admin/test/BeanFactory.java
@@ -1,5 +1,8 @@
 package no.helsebiblioteket.admin.test;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import no.helsebiblioteket.admin.dao.AccessDao;
 import no.helsebiblioteket.admin.dao.AccessTypeDao;
 import no.helsebiblioteket.admin.dao.ActionDao;
@@ -61,8 +64,11 @@ import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import org.springframework.security.userdetails.UserDetailsService;
 
+import sun.util.logging.resources.logging;
+
 public class BeanFactory {
 	private Resource res;
+    private static final Logger LOG = Logger.getLogger(BeanFactory.class.toString());
 	private XmlBeanFactory factory;
 	private static BeanFactory single;
 	public static BeanFactory factory(){
@@ -115,6 +121,20 @@ public class BeanFactory {
 	
 	// Main method when not using ant.
 	public static void main(String[] args) {
+		testDaos();
+		testServices();
+		testPerformance();
+		System.out.println("OK");
+	}
+	
+	private static void testPerformance() {
+		URLServiceTests urlServiceTests = new URLServiceTests();
+		long startTimeMillis = System.currentTimeMillis();
+		urlServiceTests.testAll();
+        LOG.log(Level.INFO, "urlServiceTests.testPerformance() took " + String.valueOf(System.currentTimeMillis() - startTimeMillis) + " milliseconds");
+	}
+	
+	private static void testDaos(){
 		AccessDaoTests accessDaoTests = new AccessDaoTests();
 		accessDaoTests.testAccess();
 		AccessTypeDaoTests accessTypeDaoTests = new AccessTypeDaoTests();
@@ -155,7 +175,9 @@ public class BeanFactory {
 		userListDaoTests.testUserList();
 		UserRoleDaoTests userRoleDaoTests = new UserRoleDaoTests();
 		userRoleDaoTests.testUserRole();
-		
+	}
+	
+	private static void testServices(){
 		AccessServiceTests accessServiceTests = new AccessServiceTests();
 		accessServiceTests.testInsertAccess();
 		
@@ -179,7 +201,6 @@ public class BeanFactory {
 		
 		UserServiceTests userServiceTests = new UserServiceTests();
 		userServiceTests.testAll();
-		
-		System.out.println("OK");
 	}
+	
 }
