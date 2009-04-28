3555f9657466340f511c07bc3231a44201df3fe3
leiftorger <leiftorger@e7bf3d78-9b82-11dd-8e58-870c635edf7a>
2009-04-28 19:50:48 +0000

    spelling mistake in class name

--- trunk/helsebiblioteket-administration/src/no/helsebiblioteket/jsf/validators/IpAddressValidatorJSF.java
+++ trunk/helsebiblioteket-administration/src/no/helsebiblioteket/jsf/validators/IpAddressValidatorJSF.java
@@ -11,7 +11,7 @@ import javax.faces.context.FacesContext;
 import javax.faces.validator.Validator;
 import javax.faces.validator.ValidatorException;
 
-public class IpAddressValidatiorJSF implements Validator {
+public class IpAddressValidatorJSF implements Validator {
 
 	@Override
 	public void validate(FacesContext arg0, UIComponent arg1, Object arg2) throws ValidatorException {
