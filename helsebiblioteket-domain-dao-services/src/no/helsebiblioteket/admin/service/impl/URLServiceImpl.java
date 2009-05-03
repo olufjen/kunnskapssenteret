6e91c78a515d6881d6565785d4b144b90a76d0fe
leiftorger <leiftorger@e7bf3d78-9b82-11dd-8e58-870c635edf7a>
2009-05-03 22:47:39 +0000

    modified method for translating URL for a given user/org and url. changed access criteria for "checkAccess" added javadoc comments

--- helsebiblioteket-domain-dao-services/src/no/helsebiblioteket/admin/service/impl/URLServiceImpl.java
+++ helsebiblioteket-domain-dao-services/src/no/helsebiblioteket/admin/service/impl/URLServiceImpl.java
@@ -1,33 +1,29 @@
 package no.helsebiblioteket.admin.service.impl;
 
-import java.util.List;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import no.helsebiblioteket.admin.service.AccessService;
 import no.helsebiblioteket.admin.service.URLService;
+import no.helsebiblioteket.admin.domain.AccessType;
 import no.helsebiblioteket.admin.domain.MemberOrganization;
+import no.helsebiblioteket.admin.domain.OrganizationType;
 import no.helsebiblioteket.admin.domain.OrganizationUser;
-import no.helsebiblioteket.admin.domain.Resource;
-import no.helsebiblioteket.admin.domain.ResourceAccess;
 import no.helsebiblioteket.admin.domain.Role;
-import no.helsebiblioteket.admin.domain.SupplierSource;
-import no.helsebiblioteket.admin.domain.SupplierSourceResource;
 import no.helsebiblioteket.admin.domain.Url;
 import no.helsebiblioteket.admin.domain.User;
 import no.helsebiblioteket.admin.domain.category.AccessTypeCategory;
 import no.helsebiblioteket.admin.domain.key.AccessTypeKey;
+import no.helsebiblioteket.admin.domain.key.UserRoleKey;
 import no.helsebiblioteket.admin.domain.list.ResourceAccessListItem;
 import no.helsebiblioteket.admin.domain.requestresult.EmptyResultString;
 import no.helsebiblioteket.admin.domain.requestresult.EmptyResultSupplierSource;
-import no.helsebiblioteket.admin.domain.requestresult.ListResultResourceAccess;
 import no.helsebiblioteket.admin.domain.requestresult.ListResultResourceAccessListItem;
-import no.helsebiblioteket.admin.domain.requestresult.ListResultSupplierSource;
 import no.helsebiblioteket.admin.domain.requestresult.SingleResultString;
 import no.helsebiblioteket.admin.domain.requestresult.SingleResultSupplierSource;
 import no.helsebiblioteket.admin.domain.requestresult.SingleResultUrl;
 import no.helsebiblioteket.admin.domain.requestresult.ValueResultString;
+import no.helsebiblioteket.admin.domain.requestresult.ValueResultSupplierSource;
 import no.helsebiblioteket.admin.domain.requestresult.ValueResultUrl;
 
 /**
@@ -46,9 +42,9 @@ public class URLServiceImpl implements URLService {
 	/**
 	 * This checks if a URL is relevant(affected) for the proxy
 	 * server. If the URL is found in the supplier sources table,
-	 * it may  be relevant for the proxy, but only if ...
-	 * 
-	 *  TODO: What are the criteria here?
+	 * it may be relevant for the proxy.
+	 * Match criteria is: The URL passed as an argument to this method
+	 * must match a "starts with" criteria for any stored URL.
 	 * 
 	 */
 	public Boolean isAffected(Url url) {
@@ -100,29 +96,62 @@ public class URLServiceImpl implements URLService {
 		return new ValueResultUrl(newUrl);
 	}
 	/**
-	 * Translates a URL for either organization of user.
-	 * If the organization has access, it will be used
-	 * because that may go around the proxy and relive
-	 * it from traffic.
+	 * Translates a URL for either organization or user.
 	 * 
-	 * TODO: An important question (since we do not have
-	 *       the lock symbol on the links yet) is whether
-	 *       someone who dosn't have access will be sent
-	 *       to the proxy or directly to the server?
-	 *       Sometimes one and sometimes the other?
+	 * Default behavior of this method is to return a proxified version of the URL
+	 * that is sent as a parameter to it.
+	 * The only exception to this rule is if a requestor is assigned access of type "proxy_exclude"
+	 * to a given resource, and if no requestor-access further down
+	 * the requester hierarchy overrides this rule.
+	 * 
+	 * The requester hierarchy is ordered as follows:
+	 * 1) User role of type "no role" (this is default access level for any requestor, both authenticated / known and not authenticated / unknown)
+	 * 2) organization type
+	 * 3) organization
+	 * 4) user role
+	 * 5) user
+	 * 
+	 * A requester is assigned the "proxy exclude" access type in two known cases:
+	 * 1) if the user/role/org has access to a given resource that exceeds what is offered through the proxy access. 
+	 * 2) if the resource authenticates requester(s) in their end based on either GeoIP or other authentication mechanism. 
+	 * 
 	 */
-	public SingleResultUrl translateUrlUserOrganization(User user, MemberOrganization organization, Url url){
+	public SingleResultUrl translateUrlUserOrganization(User user, MemberOrganization memberOrganization, Url url){
 		Url newUrl = new Url();
-		if(organization != null && hasAccessOrganization(organization, url)){
-			return this.translateUrlOrganization(organization, url);
-		} else if (user != null && hasAccessUser(user, url)){
-			return this.translateUrlUser(user, url);
-		} else {
-			// TODO: When to send directly?
-			newUrl.setStringValue(url.getStringValue());
+		boolean proxify = true;
+		
+		Role noRole = new Role();
+		noRole.setKey(UserRoleKey.no_role);
+		proxify = !proxyExclude(getAccessTypeForUserRole(noRole, url));
+		
+		if (memberOrganization != null) {
+			proxify = !proxyExclude(getAccessTypeForOrganizationType(memberOrganization.getOrganization().getType(), url));
+			proxify = !proxyExclude(getAccessTypeForMemberOrganization(memberOrganization, url));
 		}
+		
+		if (user != null) {
+			if (user.getRoleList() != null) {
+				for (Role role : user.getRoleList()) {
+					proxify = !proxyExclude(getAccessTypeForUserRole(role, url));
+				}
+			}
+			// TODO: phase2: handle user access
+		}
+		
+		newUrl.setStringValue(((proxify) ? this.proxyPrefix : "") + url.getStringValue());
+		
 		return new ValueResultUrl(newUrl);
 	}
+	
+	private boolean proxyExclude(AccessType accessType) {
+		if (accessType != null &&
+				accessType.getKey().getValue().equals(AccessTypeKey.proxy_exclude.getValue()) &&
+				accessType.getCategory().getValue().equals(AccessTypeCategory.GRANT.getValue())) {
+			return true;
+		}
+		return false;
+	}
+	
 	/**
 	 * Loads the Access list for a user and checks if the URL
 	 * is in the list.
@@ -158,20 +187,13 @@ public class URLServiceImpl implements URLService {
 	}
     private Boolean checkAccess(Url url, ResourceAccessListItem[] resourceAccesses) {
     	for (ResourceAccessListItem access : resourceAccesses) {
-    		if(url.getStringValue().equals(
-    				access.getUrl().getStringValue())){
-        		// If GRANT and general -> OK
-        		if(access.getCategory().getValue().equals(
-        				AccessTypeCategory.GRANT.getValue()) &&
-        					access.getKey().getValue().equals(
-        							AccessTypeKey.general.getValue())){
+    		if (url.getStringValue().startsWith(access.getUrl().getStringValue())) {
+        		// If GRANT -> OK
+        		if (access.getCategory().getValue().equals(AccessTypeCategory.GRANT.getValue())) {
             		return true;
         		}
         		// If DENY and general -> NOT OK
-        		if(access.getCategory().getValue().equals(
-        				AccessTypeCategory.DENY.getValue()) &&
-        				access.getKey().getValue().equals(
-        						AccessTypeKey.general.getValue())){
+        		if (access.getCategory().getValue().equals(AccessTypeCategory.DENY.getValue())) {
         			return false;
         		}
         	}
@@ -216,21 +238,49 @@ public class URLServiceImpl implements URLService {
 	/**
 	 * Finds the group value for a URL. Loads the resources, finds
 	 * the URL and the group name for it.
-	 * 
-	 * TODO: The database will be extended with a new field.
-	 *       Do not use supplierSource.getName().
 	 *  
 	 */
 	public SingleResultString group(Url url){
-		ListResultSupplierSource list = this.accessService.getSupplierSourceListAll("");
-		for (SupplierSource supplierSource : list.getList()) {
-			if(supplierSource.getUrl().getStringValue().equals(url.getStringValue())){
-    			// TODO: Is this all?
-				return new ValueResultString(supplierSource.getSupplierSourceName());
-			}
+		SingleResultSupplierSource supplierSourceResult = accessService.getSupplierSourceByUrlStartsWith(url);
+		if (supplierSourceResult instanceof ValueResultSupplierSource) {
+			return new ValueResultString(((ValueResultSupplierSource) supplierSourceResult).getValue().getProxyDatabaseName());
 		}
 		return new EmptyResultString();
 	}
+	
+	private AccessType getAccessTypeForOrganizationType(OrganizationType organizationType, Url url) {
+		AccessType accessType = null;
+		ListResultResourceAccessListItem organizationTypeAccessList = this.accessService.getAccessListByOrganizationType(organizationType);
+		accessType = getAccessTypeForResourceAccessList(url, organizationTypeAccessList);
+		return accessType;
+	}
+	
+	private AccessType getAccessTypeForMemberOrganization(MemberOrganization memberOrganization, Url url) {
+		AccessType accessType = null;
+		ListResultResourceAccessListItem organizationTypeAccessList = this.accessService.getAccessListByOrganization(memberOrganization.getOrganization());
+		accessType = getAccessTypeForResourceAccessList(url, organizationTypeAccessList);
+		return accessType;
+	}
+	
+	private AccessType getAccessTypeForUserRole(Role role, Url url) {
+		AccessType accessType = null;
+		ListResultResourceAccessListItem organizationTypeAccessList = this.accessService.getAccessListByRole(role);
+		accessType = getAccessTypeForResourceAccessList(url, organizationTypeAccessList);
+		return accessType;
+	}
+	
+	private AccessType getAccessTypeForResourceAccessList(Url url, ListResultResourceAccessListItem accessList) {
+		AccessType accessType = new AccessType();
+		for (ResourceAccessListItem access : accessList.getList()) {
+			if (url.getStringValue().startsWith(access.getUrl().getStringValue())) {
+				accessType.setCategory(access.getCategory());
+				accessType.setKey(access.getKey());
+				return accessType;
+			}
+		}
+		return null;
+    }
+	
 	public void setAccessService(AccessService accessService) {
 		this.accessService = accessService;
 	}
