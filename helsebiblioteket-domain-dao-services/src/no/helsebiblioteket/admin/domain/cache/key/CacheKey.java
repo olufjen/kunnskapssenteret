364a510354342404617465ed8a2b413877db012e
leiftorger <leiftorger@e7bf3d78-9b82-11dd-8e58-870c635edf7a>
2009-05-06 19:50:11 +0000

    equals

--- helsebiblioteket-domain-dao-services/src/no/helsebiblioteket/admin/domain/cache/key/CacheKey.java
+++ helsebiblioteket-domain-dao-services/src/no/helsebiblioteket/admin/domain/cache/key/CacheKey.java
@@ -3,7 +3,6 @@ package no.helsebiblioteket.admin.domain.cache.key;
 public enum CacheKey {
 	affectedUrlCache,
 	translateUrlUserOrganizationCache,
-	hasAccessUserOrganizationCache,
-	hasAccessOrganizationUserOrganizationCache,
+	accessTypeForUserAndMemberOrganizationCache,
 	defaultCache
 }
