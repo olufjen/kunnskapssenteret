9828ff30135824ce85792b2607b2c847de17148f
leiftorger <leiftorger@e7bf3d78-9b82-11dd-8e58-870c635edf7a>
2009-05-16 17:09:30 +0000

    errorlogging

--- trunk/helsebiblioteket-services-impl/src/no/helsebiblioteket/admin/webservice/CacheHelper.java
+++ trunk/helsebiblioteket-services-impl/src/no/helsebiblioteket/admin/webservice/CacheHelper.java
@@ -1,5 +1,8 @@
 package no.helsebiblioteket.admin.webservice;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import net.sf.ehcache.Cache;
 import net.sf.ehcache.CacheManager;
 import net.sf.ehcache.Element;
@@ -8,48 +11,49 @@ import no.helsebiblioteket.admin.domain.cache.key.CacheKey;
 
 public class CacheHelper {
 	private CacheManager cacheManager ;
+	private Log logger = LogFactory.getLog(getClass());
 	
 	public CacheHelper(){
 	}
 	
 	  
-	   private Cache getCache(CacheKey cacheKey) throws Exception {
-		   return cacheManager.getCache(cacheKey.name());
-	   }
+	private Cache getCache(CacheKey cacheKey) throws Exception {
+		return cacheManager.getCache(cacheKey.name());
+	}
 
-	   public void addCache(String key, Object element) {
-		   addCache(CacheKey.defaultCache, key, element);
-	   }
+	public void addCache(String key, Object element) {
+		addCache(CacheKey.defaultCache, key, element);
+	}
 	   
-	   public void addCache(CacheKey cacheKey, String key, Object element) {
-		   try {
-			   Cache cache = getCache(cacheKey);
-			   cache.put(new Element(key,element));
-		   } catch(Exception ex){
-		     	System.out.println(ex);
-		  }
-	   }
+	public void addCache(CacheKey cacheKey, String key, Object element) {
+		Cache cache = null;
+		try {
+			cache = getCache(cacheKey);
+		} catch (Exception e) {
+			logger.error("Exception occured when trying to get cache with cache key '" + cacheKey + "'. Exception message: " + e.getMessage());
+		}
+		cache.put(new Element(key,element));
+	}
 	   
-	   public Object findCache(String key) {
-		   return findCache(CacheKey.defaultCache, key);
-	   }
+	public Object findCache(String key) {
+		return findCache(CacheKey.defaultCache, key);
+	}
 
-	   public Object findCache(CacheKey cacheKey, String key) {
-	     try{
-	    	 //long timeStart = System.currentTimeMillis();
-	    	 Cache cache = getCache(cacheKey);
-		     Element element = cache.get(key);
-		     //System.out.println("lookup of cache element with cacheKey '" + cacheKey + "' and key '" + key + "' took " + (System.currentTimeMillis() - timeStart));
-		     if (element != null) {
-		    	 return (Object) element.getValue();		      
-		     } else {
-		       return null;
-		     }
-	     } catch(Exception ex){
-		     	System.out.println(ex);
-		     	return null;
-		  }
-	   }
+	public Object findCache(CacheKey cacheKey, String key) {
+		Element element = null;
+		try {
+	    	//long timeStart = System.currentTimeMillis();
+	    	Cache cache = getCache(cacheKey);
+		    element = cache.get(key);
+		    //System.out.println("lookup of cache element with cacheKey '" + cacheKey + "' and key '" + key + "' took " + (System.currentTimeMillis() - timeStart));
+		    if (element != null) {
+		    	return (Object) element.getValue();		      
+		    }
+		} catch(Exception e){
+			logger.error("Exception occured when trying to find cache with cache key '" + cacheKey + " and key '" + key + "'. Exception message: " + e.getMessage());
+		}
+		return element;
+	}
 
 	public void cacheFlush(Cache cache){
 		cache.flush();
@@ -58,6 +62,4 @@ public class CacheHelper {
 	public void setCacheManager(CacheManager cacheManager) {
 		this.cacheManager = cacheManager;
 	}
-	   
-
 }
