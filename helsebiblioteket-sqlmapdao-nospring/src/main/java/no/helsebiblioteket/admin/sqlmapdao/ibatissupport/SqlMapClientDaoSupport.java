caf42d7fc97bb97c5e5340ba4075ea63163e40b4
selveste.martin.solaas <selveste.martin.solaas@e7bf3d78-9b82-11dd-8e58-870c635edf7a>
2010-10-21 11:22:25 +0000

    compiles and should function as if spring was used

--- helsebiblioteket-sqlmapdao-nospring/src/main/java/no/helsebiblioteket/admin/sqlmapdao/ibatissupport/SqlMapClientDaoSupport.java
+++ helsebiblioteket-sqlmapdao-nospring/src/main/java/no/helsebiblioteket/admin/sqlmapdao/ibatissupport/SqlMapClientDaoSupport.java
@@ -1,64 +1,28 @@
-
+/**
+ * Utilities for faking Spring after Spring amputation
+ */
  
  package no.helsebiblioteket.admin.sqlmapdao.ibatissupport;
  
  import javax.sql.DataSource;
  import com.ibatis.sqlmap.client.SqlMapClient;
- 
- 
- /**
-  * Convenient super class for iBATIS SqlMapClient data access objects.
-  * Requires a SqlMapClient to be set, providing a SqlMapClientTemplate
-  * based on it to subclasses.
-  *
-  * <p>Instead of a plain SqlMapClient, you can also pass a preconfigured
-  * SqlMapClientTemplate instance in. This allows you to share your
-  * SqlMapClientTemplate configuration for all your DAOs, for example
-  * a custom SQLExceptionTranslator to use.
-  *
-  * @author Juergen Hoeller
-  * @since 24.02.2004
-  * @see #setSqlMapClient
-  * @see #setSqlMapClientTemplate
-  * @see org.springframework.orm.ibatis.SqlMapClientTemplate
-  * @see org.springframework.orm.ibatis.SqlMapClientTemplate#setExceptionTranslator
-  */
+
  public abstract class SqlMapClientDaoSupport  {
  
-     //private SqlMapClientTemplate sqlMapClientTemplate = new SqlMapClientTemplate();
-	 
-     private boolean externalTemplate = false;
- 
-     /**
-      * Set the JDBC DataSource to be used by this DAO.
-      * Not required: The SqlMapClient might carry a shared DataSource.
-      * @see #setSqlMapClient
-      */
-     public final void setDataSource(DataSource   dataSource) {
-      // this.sqlMapClientTemplate.setDataSource(dataSource);
-     }
- 
+     private SqlMapClientTemplate sqlMapClientTemplate = new SqlMapClientTemplate();
+
      /**
       * Return the JDBC DataSource used by this DAO.
       */
      public final DataSource   getDataSource() {
-      return null;//   return (this.sqlMapClientTemplate != null ? this.sqlMapClientTemplate.getDataSource() : null);
-     }
- 
-     /**
-      * Set the iBATIS Database Layer SqlMapClient to work with.
-      * Either this or a "sqlMapClientTemplate" is required.
-      * @see #setSqlMapClientTemplate
-     */
-     public final void setSqlMapClient(SqlMapClient sqlMapClient) {
-         //this.sqlMapClientTemplate.setSqlMapClient(sqlMapClient);
+      return (this.sqlMapClientTemplate != null ? this.sqlMapClientTemplate.getDataSource() : null);
      }
  
      /**
       * Return the iBATIS Database Layer SqlMapClient that this template works with.
       */
      public final SqlMapClient getSqlMapClient() {
-         return null;//return this.sqlMapClientTemplate.getSqlMapClient();
+         return this.sqlMapClientTemplate.getSqlMapClient();
      }
  
      /**
@@ -67,11 +31,10 @@
       * @see #setSqlMapClient
       */
      public final void setSqlMapClientTemplate(SqlMapClientTemplate sqlMapClientTemplate) {
-        // if (sqlMapClientTemplate == null) {
-        //     throw new IllegalArgumentException  ("Cannot set sqlMapClientTemplate to null");
-        // }
-       //  this.sqlMapClientTemplate = sqlMapClientTemplate;
-       //  this.externalTemplate = true;
+         if (sqlMapClientTemplate == null) {
+             throw new IllegalArgumentException  ("Cannot set sqlMapClientTemplate to null");
+         }
+         this.sqlMapClientTemplate = sqlMapClientTemplate;
      }
  
      /**
@@ -79,13 +42,7 @@
       * pre-initialized with the SqlMapClient or set explicitly.
       */
      public final SqlMapClientTemplate getSqlMapClientTemplate() {
-      return null;// return sqlMapClientTemplate;
-     }
- 
-     protected final void checkDaoConfig() {
-        // if (!this.externalTemplate) {
-        //     this.sqlMapClientTemplate.afterPropertiesSet();
-        // }
+      return sqlMapClientTemplate;
      }
  
  }
