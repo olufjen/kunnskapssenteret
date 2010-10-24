d08b74636013dd947c0bbee8137e922cd1663f17
selveste.martin.solaas <selveste.martin.solaas@e7bf3d78-9b82-11dd-8e58-870c635edf7a>
2010-10-24 17:18:39 +0000

    x

---
+++ helsebiblioteket-sqlmapdao-nospring/main/java/no/helsebiblioteket/admin/sqlmapdao/plugin/IbatisProofOfConceptPlugin.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package no.helsebiblioteket.admin.sqlmapdao.plugin;
+
+import com.enonic.cms.api.plugin.HttpControllerPlugin;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import no.helsebiblioteket.admin.dao.PersonDao;
+import no.helsebiblioteket.admin.domain.Person;
+import no.helsebiblioteket.admin.domain.User;
+import no.helsebiblioteket.admin.sqlmapdao.SqlMapPersonDao;
+
+public class IbatisProofOfConceptPlugin extends HttpControllerPlugin {
+
+    public void handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        String content = null;
+        User user = new User();
+        user.setUsername("hpfadmin");
+        PersonDao personDao = new SqlMapPersonDao();
+
+        Person person = personDao.getPersonByUser(user);
+
+        response.setContentType("text/plain");
+        response.setContentLength(content.getBytes().length);
+        response.getWriter().print(content);
+    }
+}
