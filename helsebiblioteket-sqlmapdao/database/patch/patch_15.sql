00863daeb18e088673617847e5fd6cf35fe0d8b3
selveste.martin.solaas@gmail.com <selveste.martin.solaas@gmail.com@e7bf3d78-9b82-11dd-8e58-870c635edf7a>
2010-12-17 14:12:19 +0000

    missing patches

---
+++ helsebiblioteket-sqlmapdao/database/patch/patch_15.sql
@@ -0,0 +1,7 @@
+insert into tbl_org_type_reg (org_type_id, descr, name, key) values (6, 'Helsebiblioteket', 'Helsebiblioteket', 'helsebiblioteket');
+
+insert into tbl_org_type_reg (org_type_id, descr, name, key) values (7, 'Primary care', 'primary care', 'primary_care');
+
+insert into tbl_org_type_reg (org_type_id, descr, name, key) values (8, 'University', 'university', 'university');
+
+update tbl_org_type_reg set descr='Government', name='government', key='government' where key = 'public_administration';
