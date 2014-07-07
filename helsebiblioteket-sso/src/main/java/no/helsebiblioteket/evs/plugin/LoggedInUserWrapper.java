ced5936e1128bf99e1decf88383ca3c0b71c54f0
rubicksman@gmail.com <rubicksman@gmail.com@e7bf3d78-9b82-11dd-8e58-870c635edf7a>
2014-07-07 08:35:36 +0000

    Added servialVersionUID to the wrapper classes.

--- helsebiblioteket-sso/src/main/java/no/helsebiblioteket/evs/plugin/LoggedInUserWrapper.java
+++ helsebiblioteket-sso/src/main/java/no/helsebiblioteket/evs/plugin/LoggedInUserWrapper.java
@@ -7,6 +7,9 @@ import no.helsebiblioteket.admin.domain.LoggedInUser;
  * Created by mla on 30/06/14.
  */
 public class LoggedInUserWrapper implements Serializable {
+
+    public static final long serialVersionUID = 1L;
+
     private transient LoggedInUser wrapped;
 
     public LoggedInUserWrapper ( final LoggedInUser wrapped ) {
