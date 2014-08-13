0a7dda60bbdd66ad741225e681b74744803e927c
olufjen@gmail.com <olufjen@gmail.com@e7bf3d78-9b82-11dd-8e58-870c635edf7a>
2014-08-13 11:06:04 +0000

    Rekonfigurering web modelklasser

--- biovigilans/biovigilans_web/src/main/java/no/naks/biovigilans/web/control/HendelseWebService.java
+++ biovigilans/biovigilans_web/src/main/java/no/naks/biovigilans/web/control/HendelseWebService.java
@@ -1,10 +1,15 @@
 package no.naks.biovigilans.web.control;
 
+import no.naks.biovigilans.service.PasientTableService;
 import no.naks.biovigilans.web.model.PasientKomplikasjonWebModel;
 import no.naks.framework.web.control.MasterWebService;
 
 public interface HendelseWebService extends MasterWebService {
 
 	public void saveHendelse(PasientKomplikasjonWebModel innMelding);
+	public PasientTableService getPasientService();
+
+
+	public void setPasientService(PasientTableService pasientService);
 	
 }
--- biovigilans/biovigilans_web/src/main/java/no/naks/biovigilans/web/control/HendelseWebServiceImpl.java
+++ biovigilans/biovigilans_web/src/main/java/no/naks/biovigilans/web/control/HendelseWebServiceImpl.java
@@ -1,22 +1,35 @@
 package no.naks.biovigilans.web.control;
 
 import no.naks.biovigilans.model.PasientGruppeImpl;
-
+import no.naks.biovigilans.service.PasientTableService;
 import no.naks.biovigilans.web.model.PasientKomplikasjonWebModel;
 import no.naks.framework.web.control.MasterWebServiceImpl;
 
 public class HendelseWebServiceImpl extends MasterWebServiceImpl implements
 		HendelseWebService{
 
+	private PasientTableService pasientService;
+	
 	public HendelseWebServiceImpl() {
 		super();
 		 System.out.println("Hendelsewebservice created");
 	}
 
+	
+	public PasientTableService getPasientService() {
+		return pasientService;
+	}
+
+
+	public void setPasientService(PasientTableService pasientService) {
+		this.pasientService = pasientService;
+	}
+
+
 	@Override
 	public void saveHendelse(PasientKomplikasjonWebModel innMelding) {
 
-		
+		pasientService.savePasient(innMelding.getPasient());
 	}
 
 
