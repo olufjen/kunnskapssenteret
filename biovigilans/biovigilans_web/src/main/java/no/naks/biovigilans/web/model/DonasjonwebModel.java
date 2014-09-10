f013b534a580fddc270cadba4522d4973ad39869
qadeeralvi@gmail.com <qadeeralvi@gmail.com@e7bf3d78-9b82-11dd-8e58-870c635edf7a>
2014-09-10 08:04:05 +0000

    donasjon

---
+++ biovigilans/biovigilans_web/src/main/java/no/naks/biovigilans/web/model/DonasjonwebModel.java
@@ -0,0 +1,48 @@
+package no.naks.biovigilans.web.model;
+
+import java.util.Map;
+
+import no.naks.biovigilans.model.Donasjon;
+import no.naks.biovigilans.model.DonasjonImpl;
+
+public class DonasjonwebModel extends VigilansModel {
+	
+	private Donasjon donasjon;
+	
+	
+	public DonasjonwebModel(){
+		super();
+		donasjon = new DonasjonImpl();
+	}
+
+
+	public Donasjon getDonasjon() {
+		return donasjon;
+	}
+
+
+	public void setDonasjon(Donasjon donasjon) {
+		this.donasjon = donasjon;
+	}
+	
+	public void distributeTerms(){
+		String[] formFields = getFormNames();
+		String donasjonFields[] = {formFields[6],formFields[7],formFields[8],formFields[9],formFields[10]};
+		donasjon.setDonasjonsfieldMaps(donasjonFields);
+	}
+	
+	public void saveValues(){
+		String[] formFields = getFormNames();
+		Map<String,String> userEntries = getFormMap(); // formMap inneholder verdier angitt av bruker
+		
+		for(String field: formFields ){
+			String userEntry = userEntries.get(field);
+			donasjon.saveField(field, userEntry);
+		}
+		
+		donasjon.saveToField();
+		
+	}	
+
+	
+}
