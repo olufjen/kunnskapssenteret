f6aa6fd14f7f4cf74662624b7aefd36ca56d505f
olufjen <olufjen@e7bf3d78-9b82-11dd-8e58-870c635edf7a>
2014-11-17 12:30:06 +0000

    Ny tabell annenkomplikasjon

---
+++ biovigilans/biovigilans_business/src/main/java/no/naks/biovigilans/model/AnnenkomplikasjonImpl.java
@@ -0,0 +1,103 @@
+package no.naks.biovigilans.model;
+
+
+/**
+ * Vigilansmelding som annen komplikasjon
+ */
+
+public class AnnenkomplikasjonImpl extends AbstractVigilansmelding{
+
+	/**
+	 * Klassifikasjon av hendelsen (beskrivelse)
+	 */
+	private String klassifikasjon;
+	/**
+	 * Klassifikasjonskode
+	 */
+	private String Klassifikasjonkode;
+	/**
+	 * Beskrivelse av hendelsen
+	 */
+	private String komplikasjonbeskrivelse;
+	/**
+	 * Hva slags hendelse er dette?
+	 */
+	private String komplikasjondefinisjon;
+	/**
+	 * Årsak til avviket
+	 */
+	private String avvikarsak;
+	/**
+	 * Under hvilken prosess skjedde hendelsen?
+	 */
+	private String hovedprosess;
+	/**
+	 * Gjennomførte eller planlagte tiltak
+	 */
+	private String tiltak;
+	/**
+	 * Kommentarer til hendelsen
+	 */
+	private String kommentar;
+	/**
+	 * Hvordan ble hendelsen oppdaget
+	 */
+	private String oppdaget;
+	public String getKlassifikasjon() {
+		return klassifikasjon;
+	}
+	public void setKlassifikasjon(String klassifikasjon) {
+		this.klassifikasjon = klassifikasjon;
+	}
+	public String getKlassifikasjonkode() {
+		return Klassifikasjonkode;
+	}
+	public void setKlassifikasjonkode(String klassifikasjonkode) {
+		Klassifikasjonkode = klassifikasjonkode;
+	}
+	public String getKomplikasjonbeskrivelse() {
+		return komplikasjonbeskrivelse;
+	}
+	public void setKomplikasjonbeskrivelse(String komplikasjonbeskrivelse) {
+		this.komplikasjonbeskrivelse = komplikasjonbeskrivelse;
+	}
+	public String getKomplikasjondefinisjon() {
+		return komplikasjondefinisjon;
+	}
+	public void setKomplikasjondefinisjon(String komplikasjondefinisjon) {
+		this.komplikasjondefinisjon = komplikasjondefinisjon;
+	}
+	public String getAvvikarsak() {
+		return avvikarsak;
+	}
+	public void setAvvikarsak(String avvikarsak) {
+		this.avvikarsak = avvikarsak;
+	}
+	public String getHovedprosess() {
+		return hovedprosess;
+	}
+	public void setHovedprosess(String hovedprosess) {
+		this.hovedprosess = hovedprosess;
+	}
+	public String getTiltak() {
+		return tiltak;
+	}
+	public void setTiltak(String tiltak) {
+		this.tiltak = tiltak;
+	}
+	public String getKommentar() {
+		return kommentar;
+	}
+	public void setKommentar(String kommentar) {
+		this.kommentar = kommentar;
+	}
+	public String getOppdaget() {
+		return oppdaget;
+	}
+	public void setOppdaget(String oppdaget) {
+		this.oppdaget = oppdaget;
+	}
+	
+	
+}
+
