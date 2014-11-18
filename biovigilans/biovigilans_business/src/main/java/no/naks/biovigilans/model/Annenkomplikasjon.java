4e21d1f5b47d2f6c07c71d9b93b30d99cba121e4
olufjen@gmail.com <olufjen@gmail.com@e7bf3d78-9b82-11dd-8e58-870c635edf7a>
2014-11-18 07:40:37 +0000

    Tilpasning nye tabeller databasen

---
+++ biovigilans/biovigilans_business/src/main/java/no/naks/biovigilans/model/Annenkomplikasjon.java
@@ -0,0 +1,37 @@
+package no.naks.biovigilans.model;
+
+import java.util.Map;
+
+public interface Annenkomplikasjon {
+	
+	
+	public String getKlassifikasjon();
+	public void setKlassifikasjon(String klassifikasjon);
+	public String getKlassifikasjonkode();
+	public void setKlassifikasjonkode(String klassifikasjonkode);
+	public String getKomplikasjonbeskrivelse();
+	public void setKomplikasjonbeskrivelse(String komplikasjonbeskrivelse);
+	public String getKomplikasjondefinisjon();
+	public void setKomplikasjondefinisjon(String komplikasjondefinisjon);
+	public String getAvvikarsak();
+	public void setAvvikarsak(String avvikarsak);
+	public String getHovedprosess();
+	public void setHovedprosess(String hovedprosess);
+	public String getTiltak();
+	public void setTiltak(String tiltak);
+	public String getKommentar();
+	public void setKommentar(String kommentar);
+	public String getOppdaget();
+	public void setOppdaget(String oppdaget);
+	public Map<String, String> getAnnenKomplikasjonsFields();
+	public void setAnnenKomplikasjonsFields(
+			Map<String, String> annenKomplikasjonsFields);
+	public String[] getKeys();
+	public void setKeys(String[] keys);
+	public void setAnnenkomplicationfieldMaps(String[]userFields);
+	public void setParams();
+	public int[] getTypes();
+	public Object[] getParams();
+	public int[] getUtypes();		
+
+}
