9699db9a3127b648f09f9d193696f70afab7cbda
olufjen@gmail.com <olufjen@gmail.com@e7bf3d78-9b82-11dd-8e58-870c635edf7a>
2014-12-10 12:44:48 +0000

    Lagring av melderid til vigilansmelding

---
+++ biovigilans/biovigilans_business/src/main/java/no/naks/biovigilans/dao/HendelsehemovigilansDAOImpl.java
@@ -0,0 +1,11 @@
+package no.naks.biovigilans.dao;
+
+import no.naks.rammeverk.kildelag.dao.AbstractAdmintablesDAO;
+import no.naks.rammeverk.kildelag.dao.Tablesupdate;
+
+public class HendelsehemovigilansDAOImpl extends AbstractAdmintablesDAO
+		implements HendelsehemovigilansDAO {
+	private Tablesupdate tablesUpdate = null;
+	
+
+}
