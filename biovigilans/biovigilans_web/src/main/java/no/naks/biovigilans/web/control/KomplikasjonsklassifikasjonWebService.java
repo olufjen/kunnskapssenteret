839864b384a661b7c669e86e5ff0816f59c5c04b
qadeeralvi@gmail.com <qadeeralvi@gmail.com@e7bf3d78-9b82-11dd-8e58-870c635edf7a>
2015-01-08 13:41:51 +0000

    komplikasjonklassifikasjon

---
+++ biovigilans/biovigilans_web/src/main/java/no/naks/biovigilans/web/control/KomplikasjonsklassifikasjonWebService.java
@@ -0,0 +1,9 @@
+package no.naks.biovigilans.web.control;
+
+import no.naks.biovigilans.model.Komplikasjonsklassifikasjon;
+
+public interface KomplikasjonsklassifikasjonWebService {
+
+	public void saveKomplikasjonsklassifikasjon(Komplikasjonsklassifikasjon komplikasjonsklassifikasjon);
+		
+}
---
+++ biovigilans/biovigilans_web/src/main/java/no/naks/biovigilans/web/control/KomplikasjonsklassifikasjonWebServiceImpl.java
@@ -0,0 +1,23 @@
+package no.naks.biovigilans.web.control;
+
+import no.naks.biovigilans.model.Komplikasjonsklassifikasjon;
+import no.naks.biovigilans.service.KomplikasjonsklassifikasjonTableService;
+
+public class KomplikasjonsklassifikasjonWebServiceImpl implements KomplikasjonsklassifikasjonWebService {
+
+	KomplikasjonsklassifikasjonTableService komplikasjonsklassifikasjonTableService;
+
+	public KomplikasjonsklassifikasjonTableService getKomplikasjonsklassifikasjonTableService() {
+		return komplikasjonsklassifikasjonTableService;
+	}
+
+	public void setKomplikasjonsklassifikasjonTableService(
+			KomplikasjonsklassifikasjonTableService komplikasjonsklassifikasjonTableService) {
+		this.komplikasjonsklassifikasjonTableService = komplikasjonsklassifikasjonTableService;
+	}
+	
+	public void saveKomplikasjonsklassifikasjon(Komplikasjonsklassifikasjon komplikasjonsklassifikasjon){
+		komplikasjonsklassifikasjonTableService.saveAnnenKomplikasjon(komplikasjonsklassifikasjon);
+	}
+	
+}
