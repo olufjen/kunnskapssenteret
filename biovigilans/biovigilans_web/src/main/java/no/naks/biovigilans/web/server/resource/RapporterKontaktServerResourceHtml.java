7c1b3b4f6f1cb78c42a6be9ef95da84a49f39b94
qadeeralvi@gmail.com <qadeeralvi@gmail.com@e7bf3d78-9b82-11dd-8e58-870c635edf7a>
2015-01-09 10:33:01 +0000

    kontaktskjema

--- biovigilans/biovigilans_web/src/main/java/no/naks/biovigilans/web/server/resource/RapporterKontaktServerResourceHtml.java
+++ biovigilans/biovigilans_web/src/main/java/no/naks/biovigilans/web/server/resource/RapporterKontaktServerResourceHtml.java
@@ -6,7 +6,15 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -214,4 +222,40 @@ public class RapporterKontaktServerResourceHtml extends SessionServerResource {
       
     }
 
+/*
+    public void sendEmail(){
+    	Properties props = new Properties();
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.socketFactory.port", "465");
+		props.put("mail.smtp.socketFactory.class",
+				"javax.net.ssl.SSLSocketFactory");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.port", "465");
+ 
+		Session session = Session.getDefaultInstance(props,
+			new javax.mail.Authenticator() {
+				protected PasswordAuthentication getPasswordAuthentication() {
+					return new PasswordAuthentication("qadeeralvi@gmail.com","XXXX");
+				}
+			});
+ 
+		try {
+ 
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress("qadeeralvi@gmail.com"));
+			message.setRecipients(Message.RecipientType.TO,
+					InternetAddress.parse("qadeeralvi@yahoo.com"));
+			message.setSubject("Testing Subject");
+			message.setText("Dear Mail Crawler," +
+					"\n\n No spam to my email, please!");
+ 
+			Transport.send(message);
+ 
+			System.out.println("Done");
+ 
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+    }
+    */
 }
