516cc11ad1519f3bd4a6c3f5710884eb1b687009
olufjen@gmail.com <olufjen@gmail.com@e7bf3d78-9b82-11dd-8e58-870c635edf7a>
2015-02-11 13:59:03 +0000

    Endringer annen komplikasjon

--- biovigilans/biovigilans_business/src/main/java/no/naks/biovigilans/dao/AnnenkomplikasjonDAOImpl.java
+++ biovigilans/biovigilans_business/src/main/java/no/naks/biovigilans/dao/AnnenkomplikasjonDAOImpl.java
@@ -1,6 +1,7 @@
 package no.naks.biovigilans.dao;
 
 import no.naks.biovigilans.model.Annenkomplikasjon;
+import no.naks.biovigilans.model.Vigilansmelding;
 import no.naks.rammeverk.kildelag.dao.AbstractAdmintablesDAO;
 import no.naks.rammeverk.kildelag.dao.TablesUpdateImpl;
 import no.naks.rammeverk.kildelag.dao.Tablesupdate;
@@ -10,12 +11,40 @@ public class AnnenkomplikasjonDAOImpl extends AbstractAdmintablesDAO implements 
 	private String insertAnnenKomplikasjonSQL;
 	private String updateAnnenKomplikasjonSQL;
 	private String annenKomplikasjonPrimaryKey ;
+	private String insertMeldingSQL;
+	private String updateMeldingSQL;
+	private String meldingPrimaryKey;
+	private String[]meldingprimarykeyTableDefs; 
 	private String[] annenKomplikasjonprimarykeyTableDefs;
 	private Tablesupdate tablesUpdate = null;
 	
 	
 	
 	
+	public String getInsertMeldingSQL() {
+		return insertMeldingSQL;
+	}
+	public void setInsertMeldingSQL(String insertMeldingSQL) {
+		this.insertMeldingSQL = insertMeldingSQL;
+	}
+	public String getUpdateMeldingSQL() {
+		return updateMeldingSQL;
+	}
+	public void setUpdateMeldingSQL(String updateMeldingSQL) {
+		this.updateMeldingSQL = updateMeldingSQL;
+	}
+	public String getMeldingPrimaryKey() {
+		return meldingPrimaryKey;
+	}
+	public void setMeldingPrimaryKey(String meldingPrimaryKey) {
+		this.meldingPrimaryKey = meldingPrimaryKey;
+	}
+	public String[] getMeldingprimarykeyTableDefs() {
+		return meldingprimarykeyTableDefs;
+	}
+	public void setMeldingprimarykeyTableDefs(String[] meldingprimarykeyTableDefs) {
+		this.meldingprimarykeyTableDefs = meldingprimarykeyTableDefs;
+	}
 	public String getUpdateAnnenKomplikasjonSQL() {
 		return updateAnnenKomplikasjonSQL;
 	}
@@ -45,17 +74,45 @@ public class AnnenkomplikasjonDAOImpl extends AbstractAdmintablesDAO implements 
 
 
 	public void saveAnnenKomplikasjon(Annenkomplikasjon annenKomplikasjon){
+		Vigilansmelding melding = (Vigilansmelding)annenKomplikasjon;
+		melding.setMeldingsdato(null);
+		melding.setMeldingParams();
+		melding.setMeldingTypes();
+		int[]meldingTypes = melding.getTypes();
+		Object[]meldingParams = melding.getParams();
+		Long id = melding.getMeldeid();
+		String meldeSQL = insertMeldingSQL;
+		if (id != null){
+			meldeSQL = updateMeldingSQL;
+			meldingTypes = melding.getUtypes();
+		}
+		tablesUpdate = new TablesUpdateImpl(getDataSource(),meldeSQL,meldingTypes);
+		tablesUpdate.insert(meldingParams);
+		if (id == null){
+			melding.setMeldeid(getPrimaryKey(meldingPrimaryKey,meldingprimarykeyTableDefs));
+			melding.setMeldingsnokkel(null);
+			melding.setMeldingParams();
+			melding.setMeldingTypes();
+			meldeSQL = updateMeldingSQL;
+			meldingTypes = melding.getUtypes();
+			meldingParams= melding.getParams();
+			tablesUpdate = new TablesUpdateImpl(getDataSource(),meldeSQL,meldingTypes);
+			tablesUpdate.insert(meldingParams);
+		}
+		id = melding.getMeldeid();
+		tablesUpdate = null;
+
 		annenKomplikasjon.setParams();
-		int[] types = annenKomplikasjon.getTypes();
-		Object[] params = annenKomplikasjon.getParams();
+		int[] atypes = annenKomplikasjon.getTypes();
+		Object[] aparams = annenKomplikasjon.getParams();
 		boolean isUpdate = annenKomplikasjon.isUpdat();
 		String sql = insertAnnenKomplikasjonSQL;
 		if(isUpdate){
 			sql = updateAnnenKomplikasjonSQL;
-			types = annenKomplikasjon.getUtypes();
+			atypes = annenKomplikasjon.getUtypes();
 		}
-		tablesUpdate = new TablesUpdateImpl(getDataSource(),sql,types);
-		tablesUpdate.insert(params);
+		tablesUpdate = new TablesUpdateImpl(getDataSource(),sql,atypes);
+		tablesUpdate.insert(aparams);
 		
 	}
 }
