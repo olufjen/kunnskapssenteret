86124caa010ca9b3c6caa71f2d7fa96127b853e1
olufjen@gmail.com <olufjen@gmail.com@e7bf3d78-9b82-11dd-8e58-870c635edf7a>
2015-02-18 13:59:16 +0000

    Henter meldinger

---
+++ biovigilans/biovigilans_web/src/main/java/no/naks/biovigilans/web/server/resource/RapporterteMeldingertServerResourceHTML.java
@@ -0,0 +1,48 @@
+package no.naks.biovigilans.web.server.resource;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.restlet.Request;
+import org.restlet.data.LocalReference;
+import org.restlet.data.MediaType;
+import org.restlet.data.Reference;
+import org.restlet.ext.freemarker.TemplateRepresentation;
+import org.restlet.representation.Representation;
+import org.restlet.resource.ClientResource;
+import org.restlet.resource.Get;
+
+public class RapporterteMeldingertServerResourceHTML extends
+		SessionServerResource {
+
+	/**
+	 * getHemovigilans
+	 * Denne rutinen henter inn nødvendige session objekter og  setter opp nettsiden for å ta i mot
+	 * en rapportert hendelse
+	 * @return
+	 */
+	@Get
+	public Representation getHemovigilans() {
+
+
+	     Reference reference = new Reference(getReference(),"..").getTargetRef();
+	     Request request = getRequest();
+	     Map<String, Object> dataModel = new HashMap<String, Object>();
+
+	     LocalReference pakke = LocalReference.createClapReference(LocalReference.CLAP_CLASS,
+                 "/hemovigilans");
+	    
+	     LocalReference localUri = new LocalReference(reference);
+	
+// Denne client resource forholder seg til src/main/resource katalogen !!!	
+	     ClientResource clres2 = new ClientResource(LocalReference.createClapReference(LocalReference.CLAP_CLASS,"/hemovigilans/rapportert_melding.html"));
+
+	        Representation pasientkomplikasjonFtl = clres2.get();
+
+	        TemplateRepresentation  templatemapRep = new TemplateRepresentation(pasientkomplikasjonFtl,dataModel,
+	                MediaType.TEXT_HTML);
+		 return templatemapRep;
+	
+	}
+	
+}
