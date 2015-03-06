bfd8858aca910c1b9d81275a1674a16e6e0ec172
olufjen@gmail.com <olufjen@gmail.com@e7bf3d78-9b82-11dd-8e58-870c635edf7a>
2015-03-06 13:50:40 +0000

    Rapoorterte meldinger fra melder

--- biovigilans/biovigilans_business/src/main/java/no/naks/biovigilans/dao/HendelsehemovigilansDAOImpl.java
+++ biovigilans/biovigilans_business/src/main/java/no/naks/biovigilans/dao/HendelsehemovigilansDAOImpl.java
@@ -1,5 +1,12 @@
 package no.naks.biovigilans.dao;
 
+import java.sql.Types;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.jdbc.core.SqlParameter;
+
 import no.naks.biovigilans.model.Vigilansmelding;
 import no.naks.rammeverk.kildelag.dao.AbstractAdmintablesDAO;
 import no.naks.rammeverk.kildelag.dao.TablesUpdateImpl;
@@ -14,7 +21,23 @@ public class HendelsehemovigilansDAOImpl extends AbstractAdmintablesDAO
 		implements HendelsehemovigilansDAO {
 	private Tablesupdate tablesUpdate = null;
 	private String updateVigilansmeldingSQL;
+	private VigilansSelect vigilansSelect = null;
+	private String[] vigilandsMeldingTableDefs;
+	private String selectvigilansMeldingSQL;
 	
+	
+	public String[] getVigilandsMeldingTableDefs() {
+		return vigilandsMeldingTableDefs;
+	}
+	public void setVigilandsMeldingTableDefs(String[] vigilandsMeldingTableDefs) {
+		this.vigilandsMeldingTableDefs = vigilandsMeldingTableDefs;
+	}
+	public String getSelectvigilansMeldingSQL() {
+		return selectvigilansMeldingSQL;
+	}
+	public void setSelectvigilansMeldingSQL(String selectvigilansMeldingSQL) {
+		this.selectvigilansMeldingSQL = selectvigilansMeldingSQL;
+	}
 	public String getUpdateVigilansmeldingSQL() {
 		return updateVigilansmeldingSQL;
 	}
@@ -32,5 +55,13 @@ public class HendelsehemovigilansDAOImpl extends AbstractAdmintablesDAO
 		
 		tablesUpdate = new TablesUpdateImpl(getDataSource(),sql,types);
 		tablesUpdate.insert(params);
+	}
+	public List<Vigilansmelding> collectMeldinger(Long melderId){
+		int type = Types.INTEGER;
+//		Map alleMeldinger = new HashMap<String,List>();
+		vigilansSelect = new VigilansSelect(getDataSource(),selectvigilansMeldingSQL,vigilandsMeldingTableDefs);
+		vigilansSelect.declareParameter(new SqlParameter(type));
+		List meldinger = vigilansSelect.execute(melderId);
+		return meldinger;
 	}
 }
