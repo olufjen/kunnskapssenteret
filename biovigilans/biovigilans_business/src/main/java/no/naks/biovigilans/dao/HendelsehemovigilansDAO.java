d4ff127df628cc9354434f35f5adee911d7483ca
olufjen@gmail.com <olufjen@gmail.com@e7bf3d78-9b82-11dd-8e58-870c635edf7a>
2015-03-06 13:50:27 +0000

    Rapoorterte meldinger fra melder

--- biovigilans/biovigilans_business/src/main/java/no/naks/biovigilans/dao/HendelsehemovigilansDAO.java
+++ biovigilans/biovigilans_business/src/main/java/no/naks/biovigilans/dao/HendelsehemovigilansDAO.java
@@ -10,6 +10,7 @@ import org.springframework.jdbc.core.SqlParameter;
 
 
 
+
 import no.naks.biovigilans.model.Vigilansmelding;
 import no.naks.rammeverk.kildelag.dao.TablesUpdateImpl;
 import no.naks.rammeverk.kildelag.dao.Tablesupdate;
@@ -29,5 +30,5 @@ public interface HendelsehemovigilansDAO {
 	public void setUpdateVigilansmeldingSQL(String updateVigilansmeldingSQL);
 	
 	public void updateVigilansMelding(Vigilansmelding melding);	
-
+	public List<Vigilansmelding> collectMeldinger(Long melderId);
 }
