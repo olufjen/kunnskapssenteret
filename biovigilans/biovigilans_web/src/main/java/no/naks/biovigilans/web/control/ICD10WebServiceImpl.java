6d7fda7faa34cacd5b21b6708d0c6c5220409ddc
olufjen@gmail.com <olufjen@gmail.com@e7bf3d78-9b82-11dd-8e58-870c635edf7a>
2015-03-12 08:11:11 +0000

    Norks ICD10

--- biovigilans/biovigilans_web/src/main/java/no/naks/biovigilans/web/control/ICD10WebServiceImpl.java
+++ biovigilans/biovigilans_web/src/main/java/no/naks/biovigilans/web/control/ICD10WebServiceImpl.java
@@ -10,21 +10,55 @@ import javax.xml.bind.Unmarshaller;
 import no.naks.biovigilans.web.xml.ICD10CMIndex;
 import no.naks.biovigilans.web.xml.Letter;
 import no.naks.biovigilans.web.xml.MainTerm;
+import no.naks.biovigilans.web.xml.no.ICD10;
+import no.naks.biovigilans.web.xml.no.KodeNivaa1;
+import no.naks.biovigilans.web.xml.no.KodeNivaa2;
+import no.naks.biovigilans.web.xml.no.TematiskGruppeNivaa1;
+import no.naks.biovigilans.web.xml.no.TematiskGruppeNivaa2;
 
 /**
  * @author olj
  * Denne klassen leser xml definisjoner av ICD10 og gjør disse tilgjengelig for søk og oppslag
  */
 public class ICD10WebServiceImpl implements ICD10WebService {
-
+/*
+ * Engelsk ICD10
+ */
 	private Unmarshaller jaxbUnmarshaller ;
 	private String xmlFillocation;
 	private File xmlFile;
 	private List<MainTerm> terms;
 	private List<Letter> letters; 
 	private ICD10CMIndex icd10Index;
+/*
+ * Norsk ICD10
+ */
+	private ICD10 noICD10;
+	private List<TematiskGruppeNivaa1> nivaa1;
+	private List<TematiskGruppeNivaa2> nivaa2;
+	private List<KodeNivaa1> kodeNivaa1;
+	private List<KodeNivaa2> kodeNivaa2;
 	
-	public ICD10WebServiceImpl() {
+	public  ICD10WebServiceImpl() {
+		super();
+		nivaa1 = new ArrayList<TematiskGruppeNivaa1>();
+		JAXBContext jaxbContext = null;
+		try {
+			jaxbContext = JAXBContext.newInstance(ICD10.class);
+		} catch (JAXBException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}  
+		try {
+			jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+		} catch (JAXBException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}  
+		 System.out.println("ICD10 norsk utgave webservice created");
+	}		
+
+	public ICD10WebServiceImpl(String eng) {
 		super();
 		terms = new ArrayList<MainTerm>();
 		
@@ -42,6 +76,46 @@ public class ICD10WebServiceImpl implements ICD10WebService {
 			e.printStackTrace();
 		}  
 		 System.out.println("ICD10 webservice created");
+	}
+
+	public ICD10 getNoICD10() {
+		return noICD10;
+	}
+
+	public void setNoICD10(ICD10 noICD10) {
+		this.noICD10 = noICD10;
+	}
+
+	public List<TematiskGruppeNivaa1> getNivaa1() {
+		return nivaa1;
+	}
+
+	public void setNivaa1(List<TematiskGruppeNivaa1> nivaa1) {
+		this.nivaa1 = nivaa1;
+	}
+
+	public List<TematiskGruppeNivaa2> getNivaa2() {
+		return nivaa2;
+	}
+
+	public void setNivaa2(List<TematiskGruppeNivaa2> nivaa2) {
+		this.nivaa2 = nivaa2;
+	}
+
+	public List<KodeNivaa1> getKodeNivaa1() {
+		return kodeNivaa1;
+	}
+
+	public void setKodeNivaa1(List<KodeNivaa1> kodeNivaa1) {
+		this.kodeNivaa1 = kodeNivaa1;
+	}
+
+	public List<KodeNivaa2> getKodeNivaa2() {
+		return kodeNivaa2;
+	}
+
+	public void setKodeNivaa2(List<KodeNivaa2> kodeNivaa2) {
+		this.kodeNivaa2 = kodeNivaa2;
 	}
 
 	public Unmarshaller getJaxbUnmarshaller() {
@@ -86,8 +160,20 @@ public class ICD10WebServiceImpl implements ICD10WebService {
 		this.letters = letters;
 	}
 
+	@Override
+	public void readXml() {
+		 xmlFile = new File(xmlFillocation);  
+		try {
+			noICD10 = (ICD10) jaxbUnmarshaller.unmarshal(xmlFile);
+		} catch (JAXBException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	//	letters = icd10Index.getLetter();
+		 nivaa1 = noICD10.getTematiskGruppeNivaa1();
+	}
 
-
+/*	
 	@Override
 	public void readXml() {
 		 xmlFile = new File(xmlFillocation);  
@@ -100,7 +186,7 @@ public class ICD10WebServiceImpl implements ICD10WebService {
 		letters = icd10Index.getLetter();
 		
 	}
-
+*/
 	@Override
 	public ICD10CMIndex getIcd10Index() {
 		// TODO Auto-generated method stub
