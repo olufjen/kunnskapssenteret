e91c50db26397d86b972bc45ed8bb67e42847164
olufjen@gmail.com <olufjen@gmail.com@e7bf3d78-9b82-11dd-8e58-870c635edf7a>
2015-03-13 13:48:52 +0000

    Administrasjonsgrensesnitt

---
+++ biovigilans/biovigilans_admin_web/src/main/java/no/naks/biovigilans_admin/web/server/resource/SaksbehandlingServerResourceHTML.java
@@ -0,0 +1,55 @@
+package no.naks.biovigilans_admin.web.server.resource;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.restlet.Request;
+import org.restlet.data.LocalReference;
+import org.restlet.data.MediaType;
+import org.restlet.data.Reference;
+import org.restlet.ext.freemarker.TemplateRepresentation;
+import org.restlet.representation.Representation;
+import org.restlet.resource.ClientResource;
+import org.restlet.resource.Get;
+
+public class SaksbehandlingServerResourceHTML extends SessionServerResource {
+	@Get
+	public Representation getHemovigilans() {
+
+
+	     Reference reference = new Reference(getReference(),"..").getTargetRef();
+	
+	     Request request = getRequest();
+
+
+	    
+
+/*
+ * En Hashmap benyttes dersom en html side henter data fra flere javaklasser.	
+ * Hver javaklasse får en id (ex pasientkomplikasjonId) som er tilgjengelig for html
+ *      
+*/	     
+	     Map<String, Object> dataModel = new HashMap<String, Object>();
+
+	     LocalReference pakke = LocalReference.createClapReference(LocalReference.CLAP_CLASS,
+                 "/hemovigilans");
+	    
+	     LocalReference localUri = new LocalReference(reference);
+	
+// Denne client resource forholder seg til src/main/resource katalogen !!!	
+	     ClientResource clres2 = new ClientResource(LocalReference.createClapReference(LocalReference.CLAP_CLASS,"/hemovigilans/saksbehandling.html"));
+	     
+	        // Load the FreeMarker template
+//	        Representation pasientkomplikasjonFtl = new ClientResource(LocalReference.createClapReference(getClass().getPackage())+ "/html/nymeldingfagprosedyre.html").get();
+//	        Representation pasientkomplikasjonFtl = new ClientResource(LocalReference.createClapReference(LocalReference.CLAP_CLASS,"/pasientkomplikasjon/nymeldingfagprosedyre.html").get();
+	        Representation pasientkomplikasjonFtl = clres2.get();
+	//        Representation pasientkomplikasjonFtl = new ClientResource("http:///no/naks/server/resource"+"/pasientkomplikasjon.ftl").get();
+	        
+//	        TemplateRepresentation  templateRep = new TemplateRepresentation(pasientkomplikasjonFtl, result,
+//	                MediaType.TEXT_HTML);
+	        TemplateRepresentation  templatemapRep = new TemplateRepresentation(pasientkomplikasjonFtl,dataModel,
+	                MediaType.TEXT_HTML);
+		 return templatemapRep;
+	 }
+
+}
