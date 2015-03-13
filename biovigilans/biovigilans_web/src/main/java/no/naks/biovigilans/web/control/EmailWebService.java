9386dc063a588cb38f6b298959c8b47e5abf9b7e
olufjen@gmail.com <olufjen@gmail.com@e7bf3d78-9b82-11dd-8e58-870c635edf7a>
2015-03-13 09:07:33 +0000

    Tilpasninger epost

--- biovigilans/biovigilans_web/src/main/java/no/naks/biovigilans/web/control/EmailWebService.java
+++ biovigilans/biovigilans_web/src/main/java/no/naks/biovigilans/web/control/EmailWebService.java
@@ -4,5 +4,5 @@ import no.naks.rammeverk.mailer.*;
 public interface EmailWebService {
 
 	public void setMailTo(String mailTo) ;
-	public void sendEmail();
+	public void sendEmail(String mailInfo);
 }
