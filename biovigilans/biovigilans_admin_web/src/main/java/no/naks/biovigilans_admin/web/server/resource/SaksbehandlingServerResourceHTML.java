187e620cda0553affc4562d05099ef1075bdc2d5
olufjen@gmail.com <olufjen@gmail.com@e7bf3d78-9b82-11dd-8e58-870c635edf7a>
2015-03-19 13:35:47 +0000

    Saksbehandling

--- biovigilans/biovigilans_admin_web/src/main/java/no/naks/biovigilans_admin/web/server/resource/SaksbehandlingServerResourceHTML.java
+++ biovigilans/biovigilans_admin_web/src/main/java/no/naks/biovigilans_admin/web/server/resource/SaksbehandlingServerResourceHTML.java
@@ -1,36 +1,50 @@
 package no.naks.biovigilans_admin.web.server.resource;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import no.naks.biovigilans.model.Vigilansmelding;
+
 import org.restlet.Request;
+import org.restlet.data.Form;
 import org.restlet.data.LocalReference;
 import org.restlet.data.MediaType;
+import org.restlet.data.Parameter;
 import org.restlet.data.Reference;
 import org.restlet.ext.freemarker.TemplateRepresentation;
 import org.restlet.representation.Representation;
 import org.restlet.resource.ClientResource;
 import org.restlet.resource.Get;
+import org.restlet.resource.Post;
 
 public class SaksbehandlingServerResourceHTML extends SessionServerResource {
+
+	private String meldeKey = "meldinger";
+	
 	@Get
 	public Representation getHemovigilans() {
 
 
-	     Reference reference = new Reference(getReference(),"..").getTargetRef();
+	    Reference reference = new Reference(getReference(),"..").getTargetRef();
 	
-	     Request request = getRequest();
-
-
-	    
-
+	    Request request = getRequest();
+	
+	    List<Vigilansmelding> meldinger = saksbehandlingWebservice.collectMessages();
+	
+	    for (Vigilansmelding melding: meldinger){
+	    	if (melding.getSjekklistesaksbehandling() == null){
+	    		melding.setSjekklistesaksbehandling("Levert");
+	    	}
+	    }
 /*
  * En Hashmap benyttes dersom en html side henter data fra flere javaklasser.	
  * Hver javaklasse får en id (ex pasientkomplikasjonId) som er tilgjengelig for html
  *      
 */	     
 	     Map<String, Object> dataModel = new HashMap<String, Object>();
-
+		 sessionAdmin.setSessionObject(getRequest(), meldinger, meldingsId);
+	     dataModel.put(meldeKey,meldinger);
 	     LocalReference pakke = LocalReference.createClapReference(LocalReference.CLAP_CLASS,
                  "/hemovigilans");
 	    
@@ -40,16 +54,54 @@ public class SaksbehandlingServerResourceHTML extends SessionServerResource {
 	     ClientResource clres2 = new ClientResource(LocalReference.createClapReference(LocalReference.CLAP_CLASS,"/hemovigilans/saksbehandling.html"));
 	     
 	        // Load the FreeMarker template
-//	        Representation pasientkomplikasjonFtl = new ClientResource(LocalReference.createClapReference(getClass().getPackage())+ "/html/nymeldingfagprosedyre.html").get();
-//	        Representation pasientkomplikasjonFtl = new ClientResource(LocalReference.createClapReference(LocalReference.CLAP_CLASS,"/pasientkomplikasjon/nymeldingfagprosedyre.html").get();
 	        Representation pasientkomplikasjonFtl = clres2.get();
-	//        Representation pasientkomplikasjonFtl = new ClientResource("http:///no/naks/server/resource"+"/pasientkomplikasjon.ftl").get();
-	        
-//	        TemplateRepresentation  templateRep = new TemplateRepresentation(pasientkomplikasjonFtl, result,
-//	                MediaType.TEXT_HTML);
+
 	        TemplateRepresentation  templatemapRep = new TemplateRepresentation(pasientkomplikasjonFtl,dataModel,
 	                MediaType.TEXT_HTML);
 		 return templatemapRep;
 	 }
+	
+	  /**
+     * storeHemovigilans
+     * Denne rutinen tar imot meldingsnøkkel fra bruker og henter frem meidngsinformasjon basert på 
+     * oppgitt meldingsnøkkel
+     * @param form
+     * @return
+     */
+    @Post
+    public Representation storeHemovigilans(Form form) {
+        Map<String, Object> dataModel = new HashMap<String, Object>();
+        Reference reference = new Reference(getReference(),"..").getTargetRef();
+  	  List<Vigilansmelding> meldinger = (List)sessionAdmin.getSessionObject(getRequest(), meldingsId);
+  	  dataModel.put(meldeKey,meldinger);
+  	  String meldingsNokkel = null;
+    	if (form != null){
+    		for (Parameter entry : form) {
+    			if (entry.getValue() != null && !(entry.getValue().equals(""))){
+    					System.out.println(entry.getName() + "=" + entry.getValue());
+    					meldingsNokkel = entry.getValue();
+    			}
+    		}
+    	}
+    	Map<String,List> meldingDetaljene = null;
+    	if (meldingsNokkel != null){
+    		 meldingDetaljene = (Map<String,List>)saksbehandlingWebservice.selectMeldinger(meldingsNokkel);
+    	}
+	     LocalReference pakke = LocalReference.createClapReference(LocalReference.CLAP_CLASS,
+              "/hemovigilans");
+	    
+	     LocalReference localUri = new LocalReference(reference);
+	
+//Denne client resource forholder seg til src/main/resource katalogen !!!	
+	     ClientResource clres2 = new ClientResource(LocalReference.createClapReference(LocalReference.CLAP_CLASS,"/hemovigilans/saksbehandling.html"));
+	     
+	        // Load the FreeMarker template
+	        Representation pasientkomplikasjonFtl = clres2.get();
+
+	        TemplateRepresentation  templatemapRep = new TemplateRepresentation(pasientkomplikasjonFtl,dataModel,
+	                MediaType.TEXT_HTML);
+		 return templatemapRep;
+    
+    }
 
 }
